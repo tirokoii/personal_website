86427fe0021f95e995f4c71b2cb8e39e2c19aef6
tilliedstrom <tilliedstrom@elev.ga.ntig.se>
2026-04-30 19:10:40 +0200

    blog content (style), temp images

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ import express from "express"
 const router = express.Router()
 
 // Startsidan
-router.get("/", (req, res, next) => {
-    res.render("index.njk")
+router.get("/", async (req, res, next) => {
+    try {
+        const [rows] = await pool.query(`
+            SELECT post.id FROM blogPost
+            ORDER BY post.created_at 
+            DESC LIMIT 4
+        `)
+        res.render("index.njk", {
+            posts: rows
+        })
+    } catch (err) {
+         res.render("index.njk", {
+            error_msg: "No posts yet",
+            posts: "none"
+         })
+    }
 })
 
 router.get("/error", (err, req, res, next) => {
